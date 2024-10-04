1aa0a69bbd6f2d16b86bcac0744df986f4bb7b81
Rafay0237 <143127689+Rafay0237@users.noreply.github.com>
2024-10-04 17:33:40 +0500

    update 3

--- controllers/uploadImage.js
+++ controllers/uploadImage.js
@@ -1,6 +1,7 @@
 const users = require("../models/user");
 const cloudinary = require("cloudinary").v2;
 const  streamifier= require("streamifier");
+const messages = require("../models/message");
 require("dotenv").config();
 
 
@@ -49,5 +50,49 @@ const updateUser=async(userId,URL)=>{
   streamifier.createReadStream(dp.buffer).pipe(stream);
 }
 
+const updateUserChat = async (userId,conversationId, URL) => {
+  let toSaveMessage = {
+    sender: userId,
+    conversationId,
+    img: URL,
+  };
+  const message = messages(toSaveMessage);
 
-module.exports={uploadImage}
+  const savedMessage = await message.save();
+  return savedMessage;
+};
+
+
+let uploadImageChat = async (req, res) => {
+  const userId = req.body.id;
+  const conversationId=req.body.conversationId;
+  const dp = req.files["image"][0];
+  console.log(dp)
+  const stream = cloudinary.uploader.upload_stream(
+    {
+      folder: "demo",
+    },
+    (error, result) => {
+      if (error) {
+        console.error("Error processing file upload:", error);
+        return res
+          .status(500)
+          .json({
+            error:error.message,
+            success: false,
+          });
+      }
+
+      const updatedChat = updateUserChat(userId,conversationId, result.secure_url);
+
+      if (!updatedChat) {
+        return res.status(400).send({ error: updatedChat, success: false });
+      }
+      res.status(200).json({img:result.secure_url,success:true});
+    }
+  );
+  streamifier.createReadStream(dp.buffer).pipe(stream);
+};
+
+
+module.exports={uploadImage, uploadImageChat}
--- routes/uploadImage.js
+++ routes/uploadImage.js
@@ -1,12 +1,14 @@
 const express=require("express")
 const multer = require("multer");
 
-const {uploadImage}=require("../controllers/uploadImage")
+const {uploadImage,uploadImageChat}=require("../controllers/uploadImage")
 
 const router=express.Router()
 const storage = multer.memoryStorage();
 const upload = multer({ storage });
 
 router.post("/",upload.fields([{ name: "image" }, { name: "id" }]),uploadImage)
+
+router.post("/chat",upload.fields([{ name: "image" }]),uploadImageChat)
 
 module.exports=router
